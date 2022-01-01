0d66e6f8b3da41bf440a38b67de6e098c776c85f
Masum-Arifin <masumarifinbd@gmail.com>
2022-01-01 19:28:23 +0600

    prime number

--- checkPrime.js
+++ checkPrime.js
@@ -1,14 +1,14 @@
 
 
-function isPrime(n){    
-for (i = 2; i < n; i++) {
-  if (n % i == 0) {
-    return "not a prime number";
-    break;
-  }
-}
-return "is a prime number";
-}
+// function isPrime(n){    
+// for (i = 2; i < n; i++) {
+//   if (n % i == 0) {
+//     return "not a prime number";
+//     break;
+//   }
+// }
+// return "is a prime number";
+// }
 var result = isPrime(139);
 console.log(result);
 var a = 5;
