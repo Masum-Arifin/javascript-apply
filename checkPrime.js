4fc2fa01c25756c040e29f15225c9ff88ef3c4da
Masum-Arifin <masumarifinbd@gmail.com>
2022-01-02 19:46:27 +0600

    prime number

--- checkPrime.js
+++ checkPrime.js
@@ -17,7 +17,7 @@ console.log("before: a =", a, "b =", b);
 var temp = a;
 a = b;
 b = temp;
-console.log("after swap: a =", a, "b =", b);
+// console.log("after swap: a =", a, "b =", b);
 var x = 5;
 var y = 7;
 x = x + y;
