d6a65e803408629b6a1363971e2f65e80da87c03
Masum-Arifin <masumarifinbd@gmail.com>
2022-01-04 20:00:53 +0600

    prime number

--- checkPrime.js
+++ checkPrime.js
@@ -37,6 +37,6 @@ var result2 = Math.ceil(num);
 var result3 = Math.round(num);
 for(var i = 0; i <10; i++){
     var randomNum = Math.random() * 6;
-    // var output = Math.round(randomNum);
+    var output = Math.round(randomNum);
     console.log(output);
 }
