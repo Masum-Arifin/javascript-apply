2bd7d2dffe1a94cff6ec5a8d650307cc02797b44
Masum-Arifin <masumarifinbd@gmail.com>
2022-01-12 20:24:07 +0600

    fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -9,7 +9,7 @@ function fibonacci(n) {
     else{
         var fibo = fibonacci(n-1);
         var nextElement = fibo[n-1] + fibo[n-2];
-        fibo.push(nextElement);
+        // fibo.push(nextElement);
         return fibo;
     }
 }
